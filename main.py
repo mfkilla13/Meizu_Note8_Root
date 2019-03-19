36d91c19f08543ca43bfb425c941ca24e10e450b
MF <mfkilla13@gmail.com>
2019-03-20 00:04:12 +0200

    Start project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pywinauto import application
 import os
-
+searchpath = r'C:\Users\User\Downloads\Note8\images'
 app = application.Application()
 path = r'C:\Program Files (x86)\Qualcomm\QPST\bin\QFIL.exe'
 app.start(path)
@@ -8,4 +8,7 @@ FlatBuildForm = app[u'WindowsForms10.Window.8.app.0.190610d_r9_ad1']
 FlatBuildForm.wait('ready')
 FlatBuildButton = FlatBuildForm.Button3
 FlatBuildButton.click()
+FlatBuildSelect = FlatBuildForm.Edit2
+FlatBuildSelect.select()
 
+[u'C:\\', u'C:\\']
