e4e224e43a45cc09bb38235798d82f7eee03e201
MF <mfkilla13@gmail.com>
2019-03-19 23:30:59 +0200

    Start project

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ import os
 app = application.Application()
 path = r'C:\Program Files (x86)\Qualcomm\QPST\bin\QFIL.exe'
 app.start(path)
+FlatBuildForm = app[u'WindowsForms10.Window.8.app.0.190610d_r9_ad1']
+FlatBuildForm.wait('ready')
+FlatBuildButton = FlatBuildForm.Button3
+FlatBuildButton.click()
+
