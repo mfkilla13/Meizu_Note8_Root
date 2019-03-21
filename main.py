5ff43c67fbdeb7534a52499c37e2debb5ab3d387
MF <mfkilla13@gmail.com>
2019-03-21 22:37:36 +0200

    Start project

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from pywinauto import application
+from pywinauto import keyboard
 import os
 searchpath = r'C:\Users\User\Downloads\Note8\images'
 app = application.Application()
 path = r'C:\Program Files (x86)\Qualcomm\QPST\bin\QFIL.exe'
 app.start(path)
-FlatBuildForm = app[u'WindowsForms10.Window.8.app.0.190610d_r9_ad1']
-FlatBuildForm.wait('ready')
-FlatBuildButton = FlatBuildForm.Button3
+qfill = app[u'WindowsForms10.Window.8.app.0.190610d_r9_ad1']
+qfill.wait('ready')
+
+FlatBuildButton = qfill.Button3
 FlatBuildButton.click()
-FlatBuildSelect = FlatBuildForm.Edit2
+
+FlatBuildSelect = qfill.Edit2
 FlatBuildSelect.select()
 
-[u'C:\\', u'C:\\']
+SelectProgPath = qfill.Button5
+SelectProgPath.click()
+
+win = app[r'Открыть файл']
+win.type_keys(searchpath+"\prog_emmc_firehose_8953_ddr.mbn")
+
+
