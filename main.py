611e2a8b0a3dae42ea6950aa3651fbf7452946ec
MF <mfkilla13@gmail.com>
2019-03-21 23:43:48 +0200

    Start project

--- main.py
+++ main.py
@@ -18,6 +18,15 @@ SelectProgPath = qfill.Button5
 SelectProgPath.click()
 
 win = app[r'Открыть файл']
-win.type_keys(searchpath+"\prog_emmc_firehose_8953_ddr.mbn")
+win.type_keys(searchpath+"\prog_emmc_firehose_8953_ddr.mbn{ENTER}")
+
+LoadXML = qfill.Button7
+LoadXML.click()
+
+win = app[r'Select RawProgramm File']
+win.type_keys("rawprogram0{ENTER}")
+
+win = app[r'Select Patch File']
+win.type_keys("patch0{ENTER}")
 
 
