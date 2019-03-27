7a6dbb499aa01b125981b6748aa55dfde537d101
MF <mfkilla13@gmail.com>
2019-03-27 22:51:57 +0200

    make runwindow function

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
-from pywinauto import application
-from pywinauto import keyboard
-import os
-searchpath = r'C:\Users\User\Downloads\Note8\images'
-app = application.Application()
+# Automatic 
+from pywinauto import application, keyboard
+
 path = r'C:\Program Files (x86)\Qualcomm\QPST\bin\QFIL.exe'
+searchpath = r'C:\Users\User\Downloads\Note8\images'
+
+def runwindow(app_name, keys):
+    """
+    Open window by Window_Name and type some text in window from keyboard
+    :param app_name: window name
+    :param keys: enter data from keyboard to window
+    """
+    win = app[app_name]
+    win.type_keys(keys)
+
+app = application.Application()
 app.start(path)
 qfill = app[u'WindowsForms10.Window.8.app.0.190610d_r9_ad1']
 qfill.wait('ready')
-
 FlatBuildButton = qfill.Button3
 FlatBuildButton.click()
-
 FlatBuildSelect = qfill.Edit2
 FlatBuildSelect.select()
-
 SelectProgPath = qfill.Button5
 SelectProgPath.click()
-
-win = app[r'Открыть файл']
-win.type_keys(searchpath+"\prog_emmc_firehose_8953_ddr.mbn{ENTER}")
-
+mbn = searchpath+"\prog_emmc_firehose_8953_ddr.mbn{ENTER}"
+runwindow(r'Открыть файл', mbn)
 LoadXML = qfill.Button7
 LoadXML.click()
-
-win = app[r'Select RawProgramm File']
-win.type_keys("rawprogram0{ENTER}")
-
-win = app[r'Select Patch File']
-win.type_keys("patch0{ENTER}")
+runwindow(r'Select RawProgramm File', "rawprogram0{ENTER}")
+runwindow(r'Select Patch File', "patch0{ENTER}")
 
 
